ca9b9adf2db7f0a327b225a4f78b17cc6702e0df
Swapna Brungi <swapna.brungi@gmail.com>
2017-05-01 23:02:28 -0400

    switched around

--- js/main.js
+++ js/main.js
@@ -1,2 +1,43 @@
+$(function(){
+//this is where the entire jQuery goes.
+
+$("div.plate > button").click(function(){
+  var button = $("div.plate > button")
+  if (button.hasClass('switch on')) {
+    button.removeClass("switch on").addClass("switch off");
+    $('body').removeClass("switch on").addClass("dark");
+    $('.status').empty().append("Hey, who turned the lights off????ouch!");
+  }
+  else {
+    button.removeClass("switch off").addClass("switch on");
+    $('body').removeClass("dark").addClass("light");
+    $('.status').empty().append("It's so bright in here!");
+  }
+});
 
 
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
